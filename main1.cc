8118d659c54e6c23e95be1ae69ed998b9cae07d6
SandoreX <rzhw3h@Gmail.com>
2019-02-01 21:12:19 +0100

    Updating scripts to execute and delete binary

--- main1.cc
+++ main1.cc
@@ -3,7 +3,13 @@
 # https://git{la,hu}b.com/sandorex
 
 # compile the file
-g++ $@ -o $0.out -xc++ $0
+g++ -o $0.out -xc++ $0
+
+# run executable
+./$0.out $@
+
+# remove executable
+rm ./$0.out
 
 # exit before trying to run c++
 exit
--- main2.cc
+++ main2.cc
@@ -4,7 +4,13 @@
 # https://git{la,hu}b.com/sandorex
 
 # cut off shebang and pipe to g++
-tail -n+2 $0 | g++ $@ -o $0.out -xc++ -
+tail -n+2 $0 | g++ -o $0.out -xc++ -
+
+# run executable
+./$0.out $@
+
+# remove executable
+rm $0.out
 
 # exit before trying to run c++
 exit
